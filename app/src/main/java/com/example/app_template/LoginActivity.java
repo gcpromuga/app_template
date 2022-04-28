24448795ff343e9e0c08cdd4f32f3d60c4004d12
Gene Romuga <gcpromuga@letran-calamba.edu.ph>
2022-04-28 14:02:27 +0800

    Add login function using email and password

--- app/src/main/java/com/example/app_template/LoginActivity.java
+++ app/src/main/java/com/example/app_template/LoginActivity.java
@@ -1,18 +1,35 @@
 package com.example.app_template;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 import java.net.InetSocketAddress;
 
 public class LoginActivity extends AppCompatActivity {
 
-    private Button btnLogin;
-    private Button btnSignUp;
+    private EditText mEmail;
+    private EditText mPassword;
+
+    private Button mLogin;
+    private Button mSignUp;
+
+    private FirebaseAuth mAuth;
+
+    private ProgressDialog mProgress;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,14 +38,14 @@ public class LoginActivity extends AppCompatActivity {
 
         initComponents();
 
-        btnLogin.setOnClickListener(new View.OnClickListener() {
+        mLogin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                startActivity(new Intent(LoginActivity.this, MainActivity.class));
+                login();
             }
         });
 
-        btnSignUp.setOnClickListener(new View.OnClickListener() {
+        mSignUp.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 startActivity(new Intent(LoginActivity.this, SignupActivity.class));
@@ -38,8 +55,54 @@ public class LoginActivity extends AppCompatActivity {
     }
 
     private void initComponents(){
-        btnLogin = (Button) findViewById(R.id.buttonLogin);
-        btnSignUp = (Button) findViewById(R.id.buttonSignUp);
+        mEmail = (EditText) findViewById(R.id.editTextEmail);
+        mPassword = (EditText) findViewById(R.id.editTextPassword);
+
+        mLogin = (Button) findViewById(R.id.buttonLogin);
+        mSignUp = (Button) findViewById(R.id.buttonSignUp);
+
+        mAuth = FirebaseAuth.getInstance();
+
+        mProgress = new ProgressDialog(this);
+    }
+
+    private void login(){
+        mProgress.setTitle("Logging in");
+        mProgress.setMessage("Please wait...");
+        mProgress.setCancelable(false);
+        mProgress.show();
+
+        String email = mEmail.getText().toString().trim();
+        String password = mPassword.getText().toString().trim();
+
+        if (TextUtils.isEmpty(email)){
+            mEmail.setError("Required field!");
+            mProgress.dismiss();
+            return;
+        }
+        if (TextUtils.isEmpty(password)){
+            mPassword.setError("Required field!");
+            mProgress.dismiss();
+            return;
+        }
+
+        mAuth.signInWithEmailAndPassword(email, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if (!task.isSuccessful()){
+                    mProgress.dismiss();
+                    Toast.makeText(getApplicationContext(), task.getException().toString(), Toast.LENGTH_SHORT).show();
+                }
+                else{
+                    if (!mAuth.getCurrentUser().isEmailVerified()) {
+                        Toast.makeText(getApplicationContext(), "Please verify your account through your email!", Toast.LENGTH_SHORT).show();
+                        mProgress.dismiss();
+                        return;
+                    }
+                    startActivity(new Intent(LoginActivity.this, MainActivity.class));
+                }
+            }
+        });
     }
 
 }
