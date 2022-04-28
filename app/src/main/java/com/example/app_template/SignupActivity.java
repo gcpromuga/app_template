33d6e312b8540359a7e2141dd886a4e840596f7f
Gene Romuga <gcpromuga@letran-calamba.edu.ph>
2022-04-28 11:29:31 +0800

    Add register user function

--- app/src/main/java/com/example/app_template/SignupActivity.java
+++ app/src/main/java/com/example/app_template/SignupActivity.java
@@ -1,18 +1,48 @@
 package com.example.app_template;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.ProgressBar;
+import android.widget.Toast;
 import android.widget.Toolbar;
+
+import com.example.app_template.object.UserDetail;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class SignupActivity extends AppCompatActivity {
 
-    private Toolbar toolbar;
-    private ImageButton imageButtonBack;
+    private Toolbar mToolbar;
+    private ImageView mBack;
+
+    private EditText mFName;
+    private EditText mLName;
+    private EditText mEmail;
+    private EditText mPassword;
+    private EditText mCPassword;
+
+    private Button mRegister;
+
+    private ProgressDialog mProgress;
+
+    private DatabaseReference mDatabase;
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,18 +51,131 @@ public class SignupActivity extends AppCompatActivity {
 
         initComponents();
 
-        imageButtonBack.setOnClickListener(new View.OnClickListener() {
+        mBack.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 startActivity(new Intent(SignupActivity.this, LoginActivity.class));
             }
         });
 
+        mRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                registerUser();
+            }
+        });
+
     }
 
     private void initComponents(){
-        toolbar = (Toolbar) findViewById(R.id.toolBarSignUp);
-        imageButtonBack = (ImageButton) findViewById(R.id.imgBack);
+        //Initialize DB Components
+        mDatabase = FirebaseDatabase.getInstance().getReference();
+        mAuth = FirebaseAuth.getInstance();
+
+        // Initialize UI Components
+        mToolbar = (Toolbar) findViewById(R.id.toolBarSignUp);
+        mBack = (ImageView) findViewById(R.id.imgBack);
+
+        mFName = (EditText) findViewById(R.id.editTextFname);
+        mLName = (EditText) findViewById(R.id.editTextLname);
+        mEmail = (EditText) findViewById(R.id.editTextEmail);
+        mPassword = (EditText) findViewById(R.id.editTextPassword);
+        mCPassword = (EditText) findViewById(R.id.editTextCPassword);
+
+        mRegister = (Button) findViewById(R.id.buttonRegister);
+
+        mProgress = mProgress = new ProgressDialog(this);
+
+    }
+
+    private void registerUser(){
+        mProgress.setTitle("Registering a user");
+        mProgress.setMessage("Please wait...");
+        mProgress.setCancelable(false);
+        mProgress.show();
+
+        String fName = mFName.getText().toString().trim();
+        String lName = mLName.getText().toString().trim();
+        String email = mEmail.getText().toString().trim();
+        String password = mPassword.getText().toString().trim();
+        String cPassword = mCPassword.getText().toString().trim();
+
+        if (TextUtils.isEmpty(fName)){
+            mFName.setError("Required field!");
+            mProgress.dismiss();
+            return;
+        }
+        if (TextUtils.isEmpty(lName)){
+            mLName.setError("Required field!");
+            mProgress.dismiss();
+            return;
+        }
+        if (TextUtils.isEmpty(email)){
+            mEmail.setError("Required field!");
+            mProgress.dismiss();
+            return;
+        }
+        if (!email.contains("@")){
+            mEmail.setError("Invalid email!");
+            mProgress.dismiss();
+            return;
+        }
+        if (TextUtils.isEmpty(password)){
+            mPassword.setError("Required field!");
+            mProgress.dismiss();
+            return;
+        }
+        if (TextUtils.isEmpty(cPassword)){
+            mCPassword.setError("Required field!");
+            mProgress.dismiss();
+            return;
+        }
+        if (!password.equals(cPassword)){
+            mCPassword.setError("Password should be equal to confirm password!");
+            mProgress.dismiss();
+            return;
+        }
+
+        mAuth.createUserWithEmailAndPassword(email, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if (!task.isSuccessful()){
+                    mProgress.dismiss();
+                    Toast.makeText(getApplicationContext(), task.getException().toString(), Toast.LENGTH_LONG).show();
+                }
+                else{
+                    final FirebaseUser user = mAuth.getCurrentUser();
+                    final String uid = mAuth.getCurrentUser().getUid();
+
+
+                    //instance of an object
+                    final UserDetail userDetail = new UserDetail();
+                    userDetail.setUid(uid);
+                    userDetail.setfName(fName);
+                    userDetail.setlName(lName);
+                    userDetail.setEmail(email.toLowerCase());
+
+                    user.sendEmailVerification().addOnCompleteListener(new OnCompleteListener<Void>() {
+                        @Override
+                        public void onComplete(@NonNull Task task) {
+                            if (!task.isSuccessful()) {
+                                mProgress.dismiss();
+                                Toast.makeText(getApplicationContext(), task.getException().toString(), Toast.LENGTH_LONG).show();
+                            }
+                            else {
+                                DatabaseReference userProfile = mDatabase.child("userProfile").child(uid);
+                                userProfile.setValue(userDetail);
+                                mProgress.dismiss();
+                                Toast.makeText(getApplicationContext(), "Please verify your account in email", Toast.LENGTH_LONG).show();
+                                startActivity(new Intent(SignupActivity.this, LoginActivity.class));
+                            }
+                        }
+                    });
+                }
+            }
+        });
+
+
     }
 
 }
---
+++ app/src/main/java/com/example/app_template/object/UserDetail.java
@@ -0,0 +1,43 @@
+package com.example.app_template.object;
+
+public class UserDetail {
+
+    private String uid;
+
+    public String getUid() {
+        return uid;
+    }
+
+    public void setUid(String uid) {
+        this.uid = uid;
+    }
+
+    public String getfName() {
+        return fName;
+    }
+
+    public void setfName(String fName) {
+        this.fName = fName;
+    }
+
+    public String getlName() {
+        return lName;
+    }
+
+    public void setlName(String lName) {
+        this.lName = lName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    private String fName;
+    private String lName;
+    private String email;
+
+}
