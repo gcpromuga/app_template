ac5d83ddf8b77666d162bda5ad7edaaa0270632b
Gene Romuga <gcpromuga@letran-calamba.edu.ph>
2022-05-01 10:28:28 +0800

    Add google sign auth

--- app/src/main/java/com/example/app_template/LoginActivity.java
+++ app/src/main/java/com/example/app_template/LoginActivity.java
@@ -1,21 +1,36 @@
 package com.example.app_template;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.text.TextUtils;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import com.google.android.gms.auth.api.Auth;
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.auth.api.signin.GoogleSignInResult;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.SignInButton;
+import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthCredential;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.GoogleAuthProvider;
 
 import java.net.InetSocketAddress;
 
@@ -26,10 +41,15 @@ public class LoginActivity extends AppCompatActivity {
 
     private Button mLogin;
     private Button mSignUp;
+    private SignInButton mGoogleSign;
+
+    private GoogleSignInClient mGoogleSignInClient;
 
     private FirebaseAuth mAuth;
 
     private ProgressDialog mProgress;
+
+    private static final int RC_SIGN_IN = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -52,6 +72,8 @@ public class LoginActivity extends AppCompatActivity {
             }
         });
 
+        goGoogleSignIn();
+
     }
 
     private void initComponents(){
@@ -60,6 +82,8 @@ public class LoginActivity extends AppCompatActivity {
 
         mLogin = (Button) findViewById(R.id.buttonLogin);
         mSignUp = (Button) findViewById(R.id.buttonSignUp);
+        mGoogleSign = findViewById(R.id.sign_in_button);
+        mGoogleSign.setSize(SignInButton.SIZE_STANDARD);
 
         mAuth = FirebaseAuth.getInstance();
 
@@ -105,4 +129,53 @@ public class LoginActivity extends AppCompatActivity {
         });
     }
 
+    private void goGoogleSignIn(){
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestIdToken(getString(R.string.web_client_id))
+                .requestEmail()
+                .build();
+
+        mGoogleSignInClient = GoogleSignIn.getClient(this, gso);
+
+        mGoogleSign.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                googleSignIn();
+            }
+        });
+    }
+
+    private void googleSignIn(){
+        Intent signInIntent = mGoogleSignInClient.getSignInIntent();
+        startActivityForResult(signInIntent, RC_SIGN_IN);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == RC_SIGN_IN) {
+            // The Task returned from this call is always completed, no need to attach
+            // a listener.
+            Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
+            handleSignInResult(task);
+        }
+    }
+
+    private void handleSignInResult(Task<GoogleSignInAccount> completedTask) {
+        try {
+            GoogleSignInAccount account = completedTask.getResult(ApiException.class);
+
+            // Signed in successfully, show authenticated UI.
+            //updateUI(account);
+            startActivity(new Intent(LoginActivity.this, MainActivity.class));
+
+        } catch (ApiException e) {
+            // The ApiException status code indicates the detailed failure reason.
+            // Please refer to the GoogleSignInStatusCodes class reference for more information.
+            Log.w("TEST", "signInResult:failed code=" + e.getStatusCode());
+            //updateUI(null);
+            Toast.makeText(getApplicationContext(), e.getStatusCode(), Toast.LENGTH_SHORT).show();
+        }
+    }
 }
--- app/src/main/java/com/example/app_template/MainActivity.java
+++ app/src/main/java/com/example/app_template/MainActivity.java
@@ -5,19 +5,30 @@ import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.drawerlayout.widget.DrawerLayout;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 import android.widget.FrameLayout;
+import android.widget.Toast;
 
 import com.google.android.material.appbar.MaterialToolbar;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.android.material.navigation.NavigationBarView;
 import com.google.android.material.navigation.NavigationView;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class MainActivity extends AppCompatActivity {
 
     private BottomNavigationView bottomNav;
     private FrameLayout frameLayout;
+
+    private FirebaseAuth mAuth;
+
+    private Button mLogout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,32 +37,50 @@ public class MainActivity extends AppCompatActivity {
 
         initComponents();
 
-        if (savedInstanceState == null) {
-            getSupportFragmentManager().beginTransaction().replace(R.id.main_framelayout,
-                    new HomeFragment()).commit();
+        if (!isAuthenticated()){
+            startActivity(new Intent(MainActivity.this, LoginActivity.class));
         }
 
-        bottomNav.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
-            @Override
-            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-                switch (item.getItemId()) {
-                    case R.id.nav_home:
-                        getSupportFragmentManager().beginTransaction().replace(R.id.main_framelayout,
-                                new HomeFragment()).commit();
-                        break;
-                    case R.id.nav_profile:
-                        getSupportFragmentManager().beginTransaction().replace(R.id.main_framelayout,
-                                new ProfileFragment()).commit();
-                        break;
-                }
-                return true;
-            }
-        });
+//        mLogout.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                mAuth.signOut();
+//                //startActivity(new Intent(MainActivity.this, LoginActivity.class));
+//            }
+//        });
+
     }
 
     private void initComponents(){
+        mLogout = (Button) findViewById(R.id.buttonLogout);
+
         bottomNav = findViewById(R.id.main_bottom_navigation);
         frameLayout = findViewById(R.id.main_framelayout);
+
+        mAuth = FirebaseAuth.getInstance();
+
+    }
+
+    private boolean isAuthenticated() {
+        FirebaseUser user = mAuth.getCurrentUser();
+//        return true;
+        if (user != null) {
+            if (user.isEmailVerified()) {
+                // do nothing
+                return true;
+            }
+            else{
+                mAuth.signOut();
+                Toast.makeText(getApplicationContext(), "Please verify your account!", Toast.LENGTH_LONG).show();
+                return false;
+                //startActivity(new Intent(MainActivity.this, LoginActivity.class));
+            }
+        }
+        else {
+            //startActivity(new Intent(MainActivity.this, LoginActivity.class));
+            return false;
+        }
+
     }
 
 }
